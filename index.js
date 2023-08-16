c32c514663545b6ce05337ee918bcf80a938b133
Jack Williams <jackmwilliams35@gmail.com>
2023-08-16 15:31:44 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names) {
+  let messages = []; // initialize an empty array to store the messages
+  for (let i = 0; i < names.length; i++) {
+    let message = `Thank you, ${names[i]}, for the wonderful surprise gift!`; // notice "surprise"
+    console.log(message);
+    messages.push(message); // add the message to the messages array
+  }
+  return messages; // return the messages array instead of names
+}
+
+writeCards(names);
+
+function countDown(num) {
+  while(num >= 0) {
+    console.log(num);
+    num--;
+  }
+}
